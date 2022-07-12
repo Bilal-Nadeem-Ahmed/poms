20d0cf3e9174ae325d856eec8cb2ce72e7683c8a
Bilal Ahmed <bilal91_@hotmail.com>
2022-07-12 16:13:24 +0100

    adds icons for edit and delete

--- components/TaskItem/index.js
+++ components/TaskItem/index.js
@@ -1,5 +1,10 @@
 const TaskItem = ({ message }) => {
-  return <p>{message}</p>;
+  return (
+    <p>
+      {message} <i class="bi text-primary bi-pencil"></i>
+      <i class="bi text-danger bi-trash3"></i>
+    </p>
+  );
 };
 
 export default TaskItem;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
+    import("bootstrap-icons/font/bootstrap-icons");
   }, []);
   return <Component {...pageProps} />;
 }
